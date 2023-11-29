7a2dafc81443d4f06de58d17194c58f7a763aa5f
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-29 22:04:42 +0200

    Add files via upload

---
+++ SimpleCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandStructure
+{
+    class SimpleCommand : ICommand
+    {
+        private string _payload = string.Empty;
+        public SimpleCommand(string payload) 
+        {   
+            this._payload = payload;
+        }
+        public void Execute() 
+        {
+            Console.WriteLine($"SimpleCommand: See, I can do simple things" + $"Like printing ({this._payload})");
+        }
+    }
+}
