baeacac282d409e4e356b1834f6c2c0843086c7a
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-29 22:03:31 +0200

    Add files via upload

---
+++ ComplexCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandStructure
+{
+    public class ComplexCommand: ICommand //ConcreteCommand2
+    {
+        private Receiver _receiver;
+
+
+        //Контекстни данни нужни за стартиране методите на получателя
+        private string _a;
+        private string _b;
+
+        public ComplexCommand(Receiver receiver, string a, string b)
+        {
+            _receiver = receiver;
+            _a = a;
+            _b = b;
+        }
+
+        public void Execute() 
+        {
+            Console.WriteLine("ComplexCommand: Complex stuff should " + $"be done by a receiver object");
+            this._receiver.DoSomething(this._a);
+            this._receiver.DoSomethingElse(this._b);
+
+        }
+    }
+}
