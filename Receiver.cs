9883e9a81ea6fc2af5875c9ff4255bfd50f607a9
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-29 22:04:33 +0200

    Add files via upload

---
+++ Receiver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandStructure
+{
+    public class Receiver  //Receiver
+    {
+        public void DoSomething(string a)
+        {
+            Console.WriteLine($"Receiver: Working on ({a}).");
+        }
+        public void DoSomethingElse( string b)
+        {
+            Console.WriteLine($"Receiver: Also working on ({b}).");
+        }
+    }
+}
