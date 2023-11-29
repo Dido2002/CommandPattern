0cc9315791cdc711cdeafabdd0bc6ef19043d399
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-29 22:04:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CommandStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {                                                                                                                                                                                                                                                                                                                                                                                                                                         
+            Invoker invoker = new Invoker();
+            invoker.SetOnStart(new SimpleCommand("Say Hi!"));
+            Receiver receiver = new Receiver();
+            invoker.SetOnFinish(new ComplexCommand(receiver, "Send email", "Save report"));
+
+            invoker.DoSomethingImportant();
+        }
+
+    }
+}
